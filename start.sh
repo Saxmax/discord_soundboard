9dc7e4593187a0ec199f7275660514b664a37cce
Niklas Näslund <naaaslund@gmail.com>
2025-10-27 20:51:18 +0100

    Change git clone repository to discord_soundboard

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ls
 rm -rf /dinmammabot
-git clone https://github.com/Saxmax/dinmammabot.git
+git clone https://github.com/Saxmax/discord_soundboard.git
 cp auth.json /dinmammabot
 cd dinmammabot
 npm install
